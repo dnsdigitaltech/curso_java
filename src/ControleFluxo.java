06b1514ba141e338c666a60fdbd9c259dec9b2c7
Davi Bernardo <dnssites@gmail.com>
2019-12-01 16:59:15 -0300

    Controle de fluxo if, else if e else

---
+++ src/ControleFluxo.java
@@ -0,0 +1,22 @@
+import java.net.SocketOption;
+
+public class ControleFluxo {
+    //estruturas condicionais IF e ELSE
+    public static void main(String[] args){
+        //idade >15 categoria infantil
+        //idade >= 15 && idade < 18 categoria juvenil
+        //idade >= 18 categoria adulta
+        int idade = 15;
+        float salario = 2000;
+        String categoria;
+        if(idade < 15) {
+            categoria = "Infantil";
+        }else if(idade >= 18){
+            categoria = "Juvenil";
+        }else{
+            categoria = "Adulta";
+        }
+
+        System.out.println(categoria);
+    }
+}
