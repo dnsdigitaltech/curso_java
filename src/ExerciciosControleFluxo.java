b069a125f1d0e149009ade11983cbcab5d0019de
Davi Bernardo <dnssites@gmail.com>
2019-12-01 18:03:54 -0300

    Controle de fluxo Resolução do exercício if else

---
+++ src/ExerciciosControleFluxo.java
@@ -0,0 +1,26 @@
+public class ExerciciosControleFluxo {
+    /*
+    Crie um variável Salário e imprima seu imposto
+    imposto:
+    salario < 1000 5%;
+    salario >= 1000 && salario < 2000 10%
+    salario >= 2000 && salario < 4000 15%
+    salario > 5000 20%
+     */
+    public static void main(String[] args) {
+        double salario = 5000;
+        double totalImposto = 0;
+        if (salario < 1000) {
+            totalImposto = salario * 0.05;
+        } else if (salario >= 1000 && salario < 2000) {
+            totalImposto = salario * 0.1;
+        } else if (salario >= 2000 && salario < 4000) {
+            totalImposto = salario * 0.15;
+        }else {
+            totalImposto = salario * 0.2;
+        }
+
+        System.out.println("O total do imposto do salario " + salario + " é " + totalImposto);
+    }
+
+}
