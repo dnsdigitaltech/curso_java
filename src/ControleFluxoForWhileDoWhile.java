768de852932c0e6385d45a6b946729ddd43a5ea8
Davi Bernardo <dnssites@gmail.com>
2019-12-02 13:33:06 -0300

    Controle de fluxo for, while, do while

---
+++ src/ControleFluxoForWhileDoWhile.java
@@ -0,0 +1,26 @@
+public class ControleFluxoForWhileDoWhile {
+    public static void main(String[] args){
+        int contador = 6;
+        //Enquanto
+        //no While se for falsa não será executado o bloco de código
+
+        while (contador < 10){
+            System.out.println(contador++);
+            //contador++; 0 a 9
+            //++contador 1 a 10
+        }
+
+        //para enquanto
+        //no Do while independe de ser falso ou verdadeiro será executado pelo menos uma vez
+        do{
+            System.out.println("dentro do while");
+        }while (contador < 10);
+
+        //para ate faça
+        //não vai avalidar apenas um instrução
+        //for(variavel inicializada; condição; incremento)
+        for(int i = 0; i<10; i++){
+            System.out.println("valor de i é: " + i);
+        }
+    }
+}
